bd6b0fff2fddb3adbe9726dcd53993c7bdd23efc
A0721I1 - Le Tran Son <letranson1992@gmail.com>
2022-10-07 18:01:08 +0700

    Update code Line Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,61 @@
 import { useEffect, useState } from "react";
 import liff from "@line/liff";
 import "./App.css";
-import React from "react";
-import ReactDOM from "react-dom";
 
 function App() {
-  const [message, setMessage] = useState("");
-  const [error, setError] = useState("");
-  // liff.ready
-  // liff.getOS()
-  // liff.getLanguage()
-  // liff.getVersion()
-  // liff.getLineVersion()
-  // liff.isInClient()
-  // liff.closeWindow()
+  const [pictureUrl, setPictureUrl] = useState("");
+  const [idToken, setIdToken] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [statusMessage, setStatusMessage] = useState("");
+  const [userId, setUserId] = useState("");
+
+  const logout = () => {
+    liff.logout();
+    window.location.reload();
+  }
+
+  const initLine = () => {
+    liff.init({ liffId: '1657488538-3Vmgrzpg' }, () => {
+      if (liff.isLoggedIn()) {
+        runApp();
+      } else {
+        liff.login();
+      }
+    }, err => console.error(err));
+  }
+
+  const runApp = () => {
+    const idToken: string | null = liff.getIDToken();
+    idToken && setIdToken(idToken);
+    liff.getProfile().then(profile => {
+      console.log(profile);  
+      setDisplayName(profile.displayName);
+      profile.pictureUrl && setPictureUrl(profile.pictureUrl);
+      profile.statusMessage && setStatusMessage(profile.statusMessage);
+      setUserId(profile.userId);
+    }).catch(err => console.error(err));
+  }
 
   useEffect(() => {
-    
-    liff
-      .init({
-        liffId: import.meta.env.VITE_LIFF_ID
-      })
-      .then(() => {
-        setMessage("LIFF init succeeded.");
-      })
-      .catch((e: Error) => {
-        setMessage("LIFF init failed.");
-        setError(`${e}`);
-      });
-    if (liff.isLoggedIn()) {
-        liff.logout();
-        window.location.reload();
-    } 
-  });
- 
+    initLine();
+  }, []);
 
-  return ( 
+  return (
     <div className="App">
-      <h1>Demo Gitec Liff App</h1>
-      {message && <p>{message}</p>}
-      {error && (
-        <p>
-          <code>{error}</code>
-        </p>
-      )}
-      <a
-        href="https://developers.line.biz/ja/docs/liff/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        LIFF Documentation
-      </a>
-      
+      <header className="App-header">
+      <div style={{ textAlign: "center" }}>
+        <h1>React with LINE Login by GITEC VN TEAM</h1>
+        <hr/>
+        <img src={pictureUrl} width="300px" height="300px"/>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>id token: </b> {idToken}</p>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>display name: </b> {displayName}</p>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>status message: </b> {statusMessage}</p>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>user id: </b> {userId}</p>
+
+        <button onClick={() => logout()} style={{ width: "100%", height: 30 }}>Logout</button>
+      </div>
+      </header>
     </div>
-    
   );
 }
 
