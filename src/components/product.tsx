2dae40df06a1fffc5380868d45d4efe2b28a74ac
A0721I1 - Le Tran Son <letranson1992@gmail.com>
2022-10-11 21:02:22 +0700

    Update code Todo App

--- src/components/product.tsx
+++
@@ -1,5 +0,0 @@
- export type productProp = {
-    id:number,
-    
-
-}
--- src/helper/fetchAPI.ts
+++
@@ -1,4 +0,0 @@
-export const getProduct = async () => {
-  const res = await fetch("https://localhost:4000/product");
-  return res.json();
-};
